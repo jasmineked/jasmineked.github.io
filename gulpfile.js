0b23e5da4963483537533996c4c7ac7e2c99569f
jasmine <>
2021-02-08 14:33:00 -0500

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,4 @@
 "use strict";
-// defining deploy task
-var gulp        = require('gulp');
-var deploy      = require('gulp-gh-pages');
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
-});
 
 // Load plugins
 const browsersync = require("browser-sync").create();
