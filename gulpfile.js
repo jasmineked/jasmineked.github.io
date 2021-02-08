85033b22c9de03a4804bdc4c24977e621b48b3cf
jasmine <>
2021-02-08 13:43:45 -0500

    define deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
 "use strict";
+// defining deploy task
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
 
 // Load plugins
 const browsersync = require("browser-sync").create();
