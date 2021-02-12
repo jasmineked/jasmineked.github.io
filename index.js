234dd04d205d92911fe3aed8b9df81dae768ffe3
jasmine <>
2021-02-12 16:38:16 -0500

    test

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 anime.timeline({loop: true})
-  // .add({
-  //   targets: '.ml5 .line',
-  //   opacity: [0.5,1],
-  //   scaleX: [0, 1],
-  //   easing: "easeInOutExpo",
-  //   duration: 700
-  // })
   .add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+  }).add({
     targets: '.ml5 .line',
     duration: 600,
     easing: "easeOutExpo",
-    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+    translateY: (el, i) => (0.625 + 0.625*2*i) + "em"
   }).add({
     targets: '.ml5 .ampersand',
     opacity: [0,1],
